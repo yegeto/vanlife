bab4f268371c6f95f10374db1c48fdc467069810
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-21 12:06:10 +0300

    Set up basic routes for Home and About pages using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1>About page goes here! 🎉</h1>;
+}
