bdf8a6d379727e396127c23fd3f82d01d8c71dbb
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-22 09:14:00 +0300

    Css file arrangements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,9 @@ export default function Home() {
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip.
       </p>
-      <Link to="vans">Find your van</Link>
+      <Link className="link-button" to="vans">
+        Find your van
+      </Link>
     </div>
   );
 }
