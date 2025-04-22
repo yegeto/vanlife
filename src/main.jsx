aa53299c83a0fbe6b0c405283f956a63b1110728
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-22 09:29:39 +0300

    Add Mirage JS mock server for van data with API routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+import { makeServer } from "./server";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
