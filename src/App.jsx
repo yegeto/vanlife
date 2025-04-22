5665c25bee298bfbde5b97b4c8af4b55d76dcf32
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-22 22:19:19 +0300

    Implement HostLayout for nested host routes with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Layout from "./components/Layout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./components/HostLayout";
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
-        <Route path="/host" element={<Dashboard />}>
+
+        <Route path="/host" element={<HostLayout />}>
+          <Route path="/host" element={<Dashboard />} />
           <Route path="/host/income" element={<Income />} />
           <Route path="/host/reviews" element={<Reviews />} />
         </Route>
