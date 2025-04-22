83cf9459a1fcc9b5c89709905281ce2c78562398
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-22 21:42:59 +0300

    Refactor routing with Layout component for shared layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Header from "./Components/Header";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./Components/Layout";
 
 function App() {
   return (
-    <>
-      {/* Navigation Links */}
-      <Header />
-
-      {/* Routes */}
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 }
 
