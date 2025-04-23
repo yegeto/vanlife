506d5ebb587f67f0bad2241325d9149482f7f6e4
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-23 22:03:35 +0300

    add footer component and integrate into layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+//Footer.jsx
+import "./Footer.css";
+
+export default function Footer() {
+  return <footer>&#169; 2025 #VANLIFE</footer>;
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,18 @@
+//Layout.jsx
+
 import { Outlet } from "react-router-dom";
+import "./Layout.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <>
+    <div className="site-wrapper">
       <Header />
-      <Outlet />
-    </>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
