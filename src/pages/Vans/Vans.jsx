45dbf4a682de11f2652eb1fda57e07c089c94214
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-24 20:51:41 +0300

    add van filtering via query parameters using Link

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,38 +1,63 @@
 import { useState, useEffect } from "react";
 import "./Vans.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+
+  const typeFilter = searchParams.get("type");
+  console.log(typeFilter);
+
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map(({ id, imageUrl, name, price, type }) => (
-    <div key={id} className="van-tile">
-      <Link
-        to={`/vans/${id}`}
-        aria-label={`View details for ${name}, priced at $${price} per day`}
-        title={`Click to view details for ${name}`}
-      >
-        <img src={imageUrl} alt={`${name} Image`} loading="lazy" />
-        <div className="van-info">
-          <h3>{name}</h3>
-          <p>
-            ${price}
-            <span>/day</span>
-          </p>
-        </div>
-        <i className={`van-type ${type} selected`}>{type}</i>
-      </Link>
-    </div>
-  ));
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
+
+  const vanElements = displayedVans.map(
+    ({ id, imageUrl, name, price, type }) => (
+      <div key={id} className="van-tile">
+        <Link
+          to={`/vans/${id}`}
+          aria-label={`View details for ${name}, priced at $${price} per day`}
+          title={`Click to view details for ${name}`}
+        >
+          <img src={imageUrl} alt={`${name} Image`} loading="lazy" />
+          <div className="van-info">
+            <h3>{name}</h3>
+            <p>
+              ${price}
+              <span>/day</span>
+            </p>
+          </div>
+          <i className={`van-type ${type} selected`}>{type}</i>
+        </Link>
+      </div>
+    )
+  );
 
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link to="?type=simple" className="van-type simple">
+          Simple
+        </Link>
+        <Link to="?type=rugged" className="van-type simple">
+          Rugged
+        </Link>
+        <Link to="?type=luxury" className="van-type simple">
+          Luxury
+        </Link>
+        <Link to="." className="van-type clear-filters">
+          Clear
+        </Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
