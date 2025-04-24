fc46622efca95c4fb89fcb221a575ed422609720
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-24 17:16:27 +0300

    Update HostVanDetail layout and style; Use relative paths in HostLayout

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,7 @@
 // HostVanDetail.jsx;
 
+import "./HostVanDetail.css";
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -18,11 +20,19 @@ export default function HostVanDetail() {
   }
 
   return (
-    <>
-      <img src={currentVan.imageUrl} alt={`Photo of ${currentVan.name}`} />
-      <h2>{currentVan.name}</h2>
-      <p>{currentVan.price}</p>
-      <p>{currentVan.type}</p>
-    </>
+    <section>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
